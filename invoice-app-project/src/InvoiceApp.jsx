f1f2a0f439adce1dd2597af1a28c9e4d4f0092b8
poeticalbyte <poeticalbyte@gmail.com>
2025-03-09 23:43:50 -0500

    Total features, minor changes.
    - Implement subtotal feature.
    - Implement IVA feature.

--- invoice-app-project/src/InvoiceApp.jsx
+++ invoice-app-project/src/InvoiceApp.jsx
@@ -1,21 +1,60 @@
+import { useEffect, useState } from "react";
+import { getInvoice } from "./services/getInvoice";
+
 export const InvoiceApp = () => {
 
+  // defaultInvoice: layout of the invoice data.
+  const defaultInvoice = {
+    id: '',
+    description: '',
+    client: {
+      firstName: '',
+      lastName: '',
+      fiscalNumber: '',
+      address: {
+        country: '',
+        city: '',
+        street: '',
+        number: ''
+      },
+    },
+    items: [],
+    company: {
+      name: 'GON-TECH INDUSTRIES',
+      fiscalNumber: '890.301.884-5',
+    },
+    total: 0,
+  };
+
+  // REACT HOOKS.
+  // useState HOOKS.
+  const [ invoice, setInvoice ] = useState(defaultInvoice);
+  const { id, description, client, company,  items, total } = invoice;
+
+  // useEffect HOOKS.
+  useEffect( () => {
+    const data = getInvoice();
+    setInvoice(data);
+  }, [] );
 
   return (
     <>
       <div className="container">
         <div className="card my-3">
 
-          <div className="card-header bg-info py-4 px-5">
+          <div className="card-header bg-primary py-4 px-5">
             {/* WEB PAGE NAME */}
             <div className="text-end"><h1>INVOICING.COM</h1></div>
             
             {/* COMPANY DETAILS */}
-            <h3>G-TECH</h3>
+            <div className="px-2">
+              <h3>{ company.name }</h3>
+              <h4>{ company.fiscalNumber }</h4>
+            </div>
             {/* INVOICE DETAILS */}
-            <ul className="list-group">
-              <li className="list-group-item"><b>ID:</b> XXXX-XXX-XXX</li>
-              <li className="list-group-item"><b>Description:</b> ...</li>
+            <ul className="list-group w-50">
+              <li className="list-group-item"><b>ID:</b> { id }</li>
+              <li className="list-group-item"><b>Description:</b> { description }</li>
             </ul>
           </div>
 
@@ -26,9 +65,9 @@ export const InvoiceApp = () => {
               <div>
                 <h3>Client details</h3>
                 <ul className="list-group">
-                  <li className="list-group-item bg-info"><b>CLIENT NAME</b></li>
-                  <li className="list-group-item">CITY, COUNTRY</li>
-                  <li className="list-group-item">STREET HOUSE NUMBER</li>
+                  <li className="list-group-item bg-primary"><b>{client.firstName} {client.lastName}</b> - C.C. {client.fiscalNumber}</li>
+                  <li className="list-group-item">{client.address.city}, {client.address.country}</li>
+                  <li className="list-group-item">{client.address.street}, {client.address.number}</li>
                 </ul>
               </div>
 
@@ -40,28 +79,52 @@ export const InvoiceApp = () => {
                 <thead>
                   <tr>
                     <th>No.</th>
-                    <th>Product</th>
+                    <th>Ref.</th>
+                    <th className="px-4">Product</th>
                     <th className="text-center">Price</th>
                     <th className="text-center">Quantity</th>
-                    <th className="text-center">Delete</th>
+                    <th className="text-center">Total</th>
+                    <th className="text-center text-danger">Delete</th>
                   </tr>
                 </thead>
                 
                 {/* TABLE BODY: CONTENT */}
                 <tbody>
-                  <tr>
+                  {
+                    items.map( ({ refe, product, price, quantity }, index) => (
+                      <tr>
+                        <td>{ index + 1 }</td>
+                        <td>{ refe }</td>
+                        <td className="px-4">{ product }</td>
+                        <td className="text-center">${ price }</td>
+                        <td className="text-center">{ quantity }</td>
+                        <td className="text-center">${ quantity * price }</td>
+                        <td className="text-center"><button className="btn btn-danger btn-sm">X</button></td>
+                      </tr>
+                    ) )
+                  }
+                  {/* <tr>
                     <td>1</td>
+                    <td>K-1561</td>
                     <td>LCD MONITOR</td>
                     <td className="text-center">500</td>
                     <td className="text-center">5</td>
                     <td className="text-center"><button className="btn btn-danger btn-sm">X</button></td>
-                  </tr>
+                  </tr> */}
                 </tbody>
               </table>
 
               <div className="row justify-content-end px-2">
-                <div className="col-8 text-end"><h3>Total</h3></div>
-                <div className="col-2 text-end"><h3 className="fw-normal"><span>2500</span></h3></div>
+                <div className="col-8 text-end"><h3>Subtotal</h3></div>
+                <div className="col-2 text-end"><h3 className="fw-normal">$<span>{(total).toFixed(2)}</span></h3></div>
+              </div>
+              <div className="row justify-content-end px-2">
+                <div className="col-8 text-end"><h3>IVA &#40;19%&#41;</h3></div>
+                <div className="col-2 text-end"><h3 className="fw-normal">$<span>{(total * 0.19).toFixed(2)}</span></h3></div>
+              </div>
+              <div className="row justify-content-end px-2">
+                <div className="col-8 text-end"><h3>TOTAL</h3></div>
+                <div className="col-2 text-end"><h3 className="fw-normal">$<span>{(total * 0.19 + total).toFixed(2)}</span></h3></div>
               </div>
 
             </div>
--- invoice-app-project/src/data/invoice.js
+++ invoice-app-project/src/data/invoice.js
@@ -1,32 +1,42 @@
+import { v4 as uuidv4 } from 'uuid';  // Import UUID
+
 // invoice: JS Object that contains all the data of the invoice.
 export const invoice = {
-  id: 0,
+  id: uuidv4(),
+  description: 'COMPRAS VARIAS',
   client: {
     firstName: 'Aldous',
     lastName: 'Caldous',
-    addres: {
-      fiscalNumber: 1234567,
+    fiscalNumber: '1234567890',
+    address: {
       country: 'Ecuador',
       city: 'Ibarra',
       street: 'Avenida San Marcos',
-      number: 32,
+      number: '32',
     },
   },
   items: [
     {
+      refe: 'P-I0709',
       product: 'Intel i7 97000k 8-core 3.6GHz',
       price: 289.99,
-      quantity: 1,
+      quantity: 2,
     },
     {
+      refe: 'K-M05',
       product: 'K100 RGB Optical-Mechanical Gaming Keyboard — CORSAIR OPX Switch',
       price: 199.99,
       quantity: 1,
     },
     {
+      refe: 'M-R07',
       product: 'ROG Swift OLED PG27UCDM',
       price: 1099.00,
-      quantity: 1,
+      quantity: 5,
     },
   ],
+  company: {
+    name: 'GON-TECH INDUSTRIES',
+    fiscalNumber: '890.301.884-7',
+  },
 };
